138ac8188a319cc4b96bfa1b58924802946702fd
Ronaldo_Oliveira <ronaldo.valenci@gmail.com>
2019-06-02 01:56:20 -0300

    mega modif

---
+++ src/exemplo_maven/megasena/AppTest.java
@@ -0,0 +1,32 @@
+package exemplo_maven.megasena;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @org.junit.Test
+    public void getNumero() {
+    }
+
+    @org.junit.Test
+    public void getJogo() {
+    }
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @Test
+    public void getNumero1() {
+    }
+
+    @Test
+    public void getJogo1() {
+    }
+
+    @Test
+    public void main1() { //
+    }
+}
